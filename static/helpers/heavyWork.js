d4bf467c7295624a995a62367e25f8c506199917
Yevhen_H <yevhen.hraivoronskyi@globallogic.com>
2022-09-08 22:17:20 +0300

    sorts of heavy work

--- static/helpers/heavyWork.js
+++ static/helpers/heavyWork.js
@@ -17,4 +17,33 @@ function runHeavyWork() {
   console.warn('finally done', i);
 }
 
-export { runHeavyWork };
+function runHeavyWorkV2() {
+  console.warn('doing heavy things...');
+
+  let i = 0;
+  console.time('heavy-stuff');
+  const partSize = 5_000_000;
+
+  const runPart = () => {
+    let localCounter = i;
+    for (/* no-op */; localCounter < i + partSize; localCounter += 1) {
+      /* no-op */
+    }
+    i = localCounter;
+
+    if (i < 4_000_000_000) {
+      // Promise.resolve().then(runPart); // не розблоковує
+      setTimeout(runPart, 0);
+    } else {
+      console.timeEnd('heavy-stuff');
+      console.warn('finally done', i);
+    }
+  };
+
+  runPart();
+}
+
+export {
+  runHeavyWork,
+  runHeavyWorkV2
+};
--- static/index.js
+++ static/index.js
@@ -8,6 +8,7 @@ import Gallery from './classes/gallery.js';
 import ProgressBar from './classes/progressBar.js';
 import { galleryItems, shuffle } from './helpers/imagesConfig.js';
 import { initModal, setModalImage } from './helpers/modal.js';
+import { runHeavyWork, runHeavyWorkV2 } from './helpers/heavyWork';
 
 initLesson('JS Lesson 18', 'Asynchronous code - Promises');
 
@@ -52,3 +53,54 @@ function getProgressLength() {
 }
 
 const blog = new Blog('lesson-title');
+
+document.getElementById('lesson-title')
+  .addEventListener('click', runHeavyWorkV2);
+
+
+// const someAsyncStuff = new Promise((resolve, reject) => {
+//   // some super-async stuff
+//   console.log('Promise is ongoing...');
+
+//   resolve('Hello from Promise');
+//   // reject(new Error('oops'));
+// });
+
+
+
+// someAsyncStuff
+//   .then((value) => console.log(value))
+//   .catch((value) => console.warn(value))
+//   .finally(() => {
+//     console.log('finally...')
+//   });
+
+// someAsyncStuff.catch(() => console.log('another catch'));
+
+// setTimeout(() => {
+//   console.log('Hello from timer')
+// }, 0);
+
+// console.log('End of sync code');
+
+// Web worker - окрема сутність з власним незалежним івент лупом і глобалами. В ньому можна робити будь-які важкі речі,
+// і це не буде блокувати основний івент луп, рендерінг, і так далі.
+// Web worker не має доступу до html, єдиний спосіб взаємодії з ним - відправка команд через _postMessage_
+// і отримання відповідей через _onmessage_
+
+// Створюємо воркер на базі окремого файла - не на базі простого імпорта функції!
+const worker = new Worker('./worker.js', { type: 'module' });
+
+// можна відправляти будь-які дані - строки, об'єкти... вони будуть склоновані і копії передані в воркер
+console.log('Main thread:', 'Asking worker to run a few tasks');
+
+worker.postMessage({ command: 'run-heavy-work' });
+worker.postMessage({ command: 'do-a-barrel' });
+
+// один івент хендлер на всі мессаджі
+worker.onmessage = (event) => {
+  // отримуєм відповідь з воркера
+  console.log('Main thread:', event.data);
+}
+
+console.log('Main thread:', 'End');
