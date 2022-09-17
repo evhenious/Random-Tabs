f2a1304e01cc707542be149924f84fd13a23e401
evhenious <evhenious@gmail.com>
2022-09-17 20:17:50 +0300

    esbuild config dev extracted to js

---
+++ esbuild-serve-dev.js
@@ -0,0 +1,19 @@
+/*
+  This is the separate esbuild config-in-js for running dev server
+*/
+const { serve } = require('esbuild');
+
+serve(
+  { servedir: './dist' },
+  {
+    entryPoints: {
+      index: './static/index.js',
+      style: './static/index.css',
+      worker: './static/web_workers/worker.js',
+    },
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    outdir: './dist',
+  }
+);
